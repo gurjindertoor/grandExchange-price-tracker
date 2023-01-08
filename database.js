1dcb889f914befeff9ae6a3775eb80b6dcec0da3
gurjindertoor <toortalk@gmail.com>
2023-01-07 22:19:46 -0600

    Changes to Update/Delete

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const axios = require('axios');
+const fs = require('fs');
 
 const ITEM_INFO_URL = "https://prices.runescape.wiki/api/v1/osrs/mapping";
 const ITEM_PRICES_URL = "https://prices.runescape.wiki/api/v1/osrs/latest";
@@ -72,6 +73,37 @@ function retrieveData() {
             if (err) console.log(err);
         })
 };
+
+//updating Data
+try {
+    const data = fs.readFileSync('updateData.JSON', 'utf8');
+    if (data) {
+        let sqlUpdate = `UPDATE grandExchange SET timestamp = ? item_id  = ? item_name = ? high_price = ? low_price = ? WHERE timestamp = ? item_id = ? item_name = ? high_price = ? low_price = ?`
+
+        db.run(sqlUpdate, data, (err) => {
+            if (err) console.log(err);
+            console.log(`Row(s) updated: ${this.changes}`)
+        })
+    }
+} catch (err) {
+    if (err) console.log(err);
+}
+
+//deleting Data
+try {
+    const data = fs.readFileSync('deleteData.JSON', 'utf8');
+    if (data) {
+        let sqlDelete = `UPDATE grandExchange SET timestamp = ? item_id  = ? item_name = ? high_price = ? low_price = ? WHERE timestamp = ? item_id = ? item_name = ? high_price = ? low_price = ?`
+
+        db.run(sqlDelete, data, (err) => {
+            if (err) console.log(err);
+            console.log(`Row(s) updated: ${this.changes}`)
+        })
+    }
+} catch (err) {
+    if (err) console.log(err);
+}
+
 
 // module.exports = retrieveData;
 const insertDataToDB = setInterval(retrieveData, INTERVAL_TIME_MILLIS);
