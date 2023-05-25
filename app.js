9d3ece96adc4f0b4b63a3ef48f4b6a401e7f5c03
gurjindertoor <78512847+gurjindertoor@users.noreply.github.com>
2023-05-24 21:58:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,285 +1,211 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const sqlite3 = require("sqlite3").verbose();
-const itemPairs = require("./item_info/itemID.js");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const itemPairs = require('./item_info/itemID.js');
 
 let currTime = Date.now();
-let oneWeekAgo = currTime - 60 * 60 * 24 * 1000 * 7;
-let lastDay = currTime - 60 * 60 * 24 * 1000;
+let oneWeekAgo = currTime - (60 * 60 * 24 * 1000 * 7);
+let lastDay = currTime - (60 * 60 * 24 * 1000);
+const resultsPerPage = 50;
 
 const app = express();
 
 const PORT = 80;
 
-const db = new sqlite3.Database(
-  "./database/ge.db",
-  sqlite3.OPEN_READONLY,
-  (err) => {
+// Initalizing the SQLite database
+const db = new sqlite3.Database('./database/ge.db', sqlite3.OPEN_READONLY, (err) => {
     if (err) console.log(err);
     console.log("Database connection: successful");
-  }
-);
+})
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
-app.use("/public", express.static("public"));
-app.use(express.static(path.join(__dirname, "./public")));
+// Setting up the View Engine as EJS and defining the views and public directories
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use('/public', express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// Handling GET request for the home page
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
-app.get("/search", (req, res) => {
-  let itemTimestamp = [],
-    itemHighPrice = [],
-    itemLowPrice = [],
-    itemInfo = [];
+// Handling GET request for search. The request includes a itemName query parameter
+app.get('/search', (req, res) => {
+    let itemTimestamp = [], itemHighPrice = [], itemLowPrice = [], itemInfo = [];
 
-  let item_name = "";
-  let recentTime = "";
+    let item_name = '';
+    let recentTime = '';
 
-  let itemNumber;
+    let itemNumber;
 
-  const itemNameQuery = req.query.itemName;
-  const itemCorrected =
-    itemNameQuery.charAt(0).toUpperCase() + itemNameQuery.slice(1);
+    const itemNameQuery = req.query.itemName;
+    const itemCorrected = itemNameQuery.charAt(0).toUpperCase() + itemNameQuery.slice(1);
 
-  const itemEntries = Object.entries(itemPairs);
-  for (const [key, value] of itemEntries) {
-    if (itemCorrected === key) {
-      itemNumber = value;
+    const itemEntries = Object.entries(itemPairs);
+    for (const [key, value] of itemEntries) {
+        if (itemCorrected === key) {
+            itemNumber = value;
+        }
     }
-  }
 
-  let sqlGrabInfo = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} AND timestamp > ${oneWeekAgo}`;
+    let sqlGrabInfo = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} AND timestamp > ${oneWeekAgo}`;
 
-  let sqlGrabRecent = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} ORDER BY timestamp DESC LIMIT 1`;
+    let sqlGrabRecent = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} ORDER BY timestamp DESC LIMIT 1`;
 
-  db.all(sqlGrabInfo, [], (err, row) => {
-    if (err) console.log(err);
-    if (row !== undefined) {
-      for (const rowItem of row) {
-        const date = new Date(rowItem.timestamp);
-
-        const [month, day, year] = [
-          date.getMonth() + 1,
-          date.getDate(),
-          date.getFullYear(),
-        ];
-
-        const [hour, minutes, seconds] = [
-          date.getHours(),
-          date.getMinutes(),
-          date.getSeconds(),
-        ];
-
-        const timeString =
-          hour.toString().padStart(2, "0") +
-          ":" +
-          minutes.toString().padStart(2, "0") +
-          ":" +
-          seconds.toString().padStart(2, "0");
-
-        const dateString = `${year}-${month}-${day} ${timeString}`;
-
-        itemHighPrice.push(rowItem.high_price);
-        itemLowPrice.push(rowItem.low_price);
-        itemTimestamp.push(dateString);
-      }
-
-      db.get(sqlGrabRecent, [], (err, row) => {
+    db.all(sqlGrabInfo, [], (err, row) => {
         if (err) console.log(err);
-        const milliseconds = row.timestamp;
-        const dateObject = new Date(milliseconds);
-        const humanDateFormat = dateObject.toLocaleString();
+        if (row !== undefined) {
+            for (const rowItem of row) {
+                const date = new Date(rowItem.timestamp);
 
-        itemInfo.push(row);
-        item_name = row.item_name;
-        recentTime = humanDateFormat;
+                const [month, day, year] = [date.getMonth() + 1, date.getDate(), date.getFullYear()];
 
-        res.render("search", {
-          labels: itemTimestamp,
-          dataHighPrice: itemHighPrice,
-          dataLowPrice: itemLowPrice,
-          itemID: item_name,
-          recentTime: recentTime,
-          dataInfo: itemInfo,
-        });
-      });
-    } else {
-      res.render("error");
-    }
-  });
+                const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+
+                const timeString = hour.toString().padStart(2, '0')
+                    + ':' + minutes.toString().padStart(2, '0')
+                    + ':' + seconds.toString().padStart(2, '0');
+
+                const dateString = `${year}-${month}-${day} ${timeString}`
+
+                itemHighPrice.push(rowItem.high_price);
+                itemLowPrice.push(rowItem.low_price);
+                itemTimestamp.push(dateString);
+            }
+
+
+            db.get(sqlGrabRecent, [], (err, row) => {
+                if (err) console.log(err);
+                const milliseconds = row.timestamp;
+                const dateObject = new Date(milliseconds);
+                const humanDateFormat = dateObject.toLocaleString();
+
+                itemInfo.push(row);
+                item_name = row.item_name;
+                recentTime = humanDateFormat;
+
+                res.render('search', { labels: itemTimestamp, dataHighPrice: itemHighPrice, dataLowPrice: itemLowPrice, itemID: item_name, recentTime: recentTime, dataInfo: itemInfo })
+            })
+        } else {
+            res.render('error');
+        }
+    })
 });
 
-const resultsPerPage = 50;
+// Handling GET request for items. The request includes a page query parameter
+app.get('/items', (req, res) => {
+    let sql_lookup = `SELECT DISTINCT item_name, item_id, high_price, low_price FROM grandExchange`;
+    db.all(sql_lookup, [], (err, row) => {
+        if (err) console.log(err);
+        const numOfResults = row.length;
+        const numOfPages = Math.ceil(numOfResults / resultsPerPage);
+        let page = req.query.page ? Number(req.query.page) : 1;
+        if (page > numOfPages) {
+            return res.redirect('?page=' + encodeURIComponent(numOfPages.toString()));
+        } else if (page < 1) {
+            return res.redirect('?page=' + encodeURIComponent('1'));
+        }
 
-app.get("/items", (req, res) => {
-  const page = req.query.page ? Number(req.query.page) : 1;
-  const startLimit = (page - 1) * resultsPerPage;
+        const startLimit = (page - 1) * resultsPerPage;
+        sql_lookup = `SELECT DISTINCT item_name, item_id, high_price, low_price FROM grandExchange LIMIT ${startLimit}, ${resultsPerPage}`;
+        db.all(sql_lookup, [], (err, row) => {
+            if (err) console.log(err);
+            let iterator = (page - 5) < 1 ? 1 : page - 5;
+            let endLink = (iterator + 9) <= numOfPages ? (iterator + 9) : page + (numOfPages - page);
+            if (endLink < (page + 4)) {
+                iterator -= (page + 4) - numOfPages;
+            }
+            res.render('items', { data: row, page, iterator, endLink, numOfPages });
+        })
+    })
+})
 
-  const sql_lookup = `
-    SELECT item_name, item_id, high_price, low_price
-    FROM grandExchange
-    GROUP BY item_name
-    ORDER BY item_name ASC
-    LIMIT ${startLimit}, ${resultsPerPage}
-  `;
+// Handling GET request for specific item by id
+app.get('/item/:id', (req, res) => {
+    let itemTimestamp = [], itemHighPrice = [], itemLowPrice = [], itemInfo = [], lastDayHighPrices = [], lastDayLowPrices = [];
 
-  db.all(sql_lookup, [], (err, items) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send("Internal Server Error");
+    let item_name = '';
+    let recentTime = '';
+
+    let itemNumber, highAverage, lowAverage;
+    let tempHigh = 0, tempLow = 0;
+
+    const itemID = req.params.id;
+    const itemCorrected = itemID.charAt(0).toUpperCase() + itemID.slice(1);
+
+    const itemEntries = Object.entries(itemPairs);
+    for (const [key, value] of itemEntries) {
+        if (itemCorrected === key) {
+            itemNumber = value;
+        }
     }
 
-    const countQuery = `
-      SELECT COUNT(DISTINCT item_name) AS count
-      FROM grandExchange
-    `;
+    let sqlGrabInfo = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} AND timestamp > ${oneWeekAgo} ORDER BY timestamp ASC`
 
-    db.get(countQuery, [], (err, row) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("Internal Server Error");
-      }
+    let sqlPriceChange = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} AND timestamp > ${lastDay}`;
 
-      const numOfResults = row.count;
-      const numOfPages = Math.ceil(numOfResults / resultsPerPage);
+    let sqlGrabRecent = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} ORDER BY timestamp DESC LIMIT 1`
 
-      if (page > numOfPages) {
-        return res.redirect(
-          "/items?page=" + encodeURIComponent(numOfPages.toString())
-        );
-      } else if (page < 1) {
-        return res.redirect("/items?page=" + encodeURIComponent("1"));
-      }
 
-      let iterator = page - 5 < 1 ? 1 : page - 5;
-      let endLink =
-        iterator + 9 <= numOfPages ? iterator + 9 : page + (numOfPages - page);
-      if (endLink < page + 4) {
-        iterator -= page + 4 - numOfPages;
-      }
+    db.all(sqlGrabInfo, [], (err, row) => {
+        if (err) console.log(err);
+        for (const rowItem of row) {
+            const date = new Date(rowItem.timestamp);
 
-      res.render("items", {
-        data: items,
-        page,
-        iterator,
-        endLink,
-        numOfPages,
-      });
-    });
-  });
-});
+            const [month, day, year] = [date.getMonth() + 1, date.getDate(), date.getFullYear()];
 
-app.get("/item/:id", (req, res) => {
-  let itemTimestamp = [],
-    itemHighPrice = [],
-    itemLowPrice = [],
-    itemInfo = [],
-    lastDayHighPrices = [],
-    lastDayLowPrices = [];
+            const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
 
-  let item_name = "";
-  let recentTime = "";
+            const timeString = hour.toString().padStart(2, '0')
+                + ':' + minutes.toString().padStart(2, '0')
+                + ':' + seconds.toString().padStart(2, '0');
 
-  let itemNumber, highAverage, lowAverage;
-  let tempHigh = 0,
-    tempLow = 0;
+            const dateString = `${year}-${month}-${day} ${timeString}`
 
-  const itemID = req.params.id;
-  const itemCorrected = itemID.charAt(0).toUpperCase() + itemID.slice(1);
+            itemHighPrice.push(rowItem.high_price);
+            itemLowPrice.push(rowItem.low_price);
+            itemTimestamp.push(dateString);
+        }
 
-  const itemEntries = Object.entries(itemPairs);
-  for (const [key, value] of itemEntries) {
-    if (itemCorrected === key) {
-      itemNumber = value;
-    }
-  }
+        db.all(sqlPriceChange, [], (err, row) => {
+            for (const rowItem of row) {
+                lastDayHighPrices.push(rowItem.high_price);
+                lastDayLowPrices.push(rowItem.low_price)
+            }
 
-  let sqlGrabInfo = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} ORDER BY timestamp ASC`;
+            for (let i = 0; i < lastDayHighPrices.length; i++) {
+                tempHigh += lastDayHighPrices[i];
+                tempLow += lastDayLowPrices[i];
 
-  let sqlPriceChange = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber}`;
+            }
 
-  let sqlGrabRecent = `SELECT timestamp, item_id, item_name, high_price, low_price FROM grandExchange WHERE item_ID = ${itemNumber} ORDER BY timestamp DESC LIMIT 1`;
+            highAverage = Math.floor(tempHigh / lastDayHighPrices.length);
+            lowAverage = Math.floor(tempLow / lastDayLowPrices.length);
+        })
 
-  db.all(sqlGrabInfo, [], (err, row) => {
-    if (err) console.log(err);
-    for (const rowItem of row) {
-      const date = new Date(rowItem.timestamp);
+        db.get(sqlGrabRecent, [], (err, row) => {
+            if (err) console.log(err);
+            const milliseconds = row.timestamp;
+            const dateObject = new Date(milliseconds);
+            const humanDateFormat = dateObject.toLocaleString();
+            itemInfo.push(row);
+            item_name = row.item_name;
+            recentTime = humanDateFormat;
 
-      const [month, day, year] = [
-        date.getMonth() + 1,
-        date.getDate(),
-        date.getFullYear(),
-      ];
+            res.render('search', { labels: itemTimestamp, dataHighPrice: itemHighPrice, dataLowPrice: itemLowPrice, itemID: item_name, recentTime: recentTime, dataInfo: itemInfo, highAvg: highAverage, lowAvg: lowAverage })
+        })
+    })
+})
 
-      const [hour, minutes, seconds] = [
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-      ];
-
-      const timeString =
-        hour.toString().padStart(2, "0") +
-        ":" +
-        minutes.toString().padStart(2, "0") +
-        ":" +
-        seconds.toString().padStart(2, "0");
-
-      const dateString = `${year}-${month}-${day} ${timeString}`;
-
-      itemHighPrice.push(rowItem.high_price);
-      itemLowPrice.push(rowItem.low_price);
-      itemTimestamp.push(dateString);
-    }
-
-    db.all(sqlPriceChange, [], (err, row) => {
-      for (const rowItem of row) {
-        lastDayHighPrices.push(rowItem.high_price);
-        lastDayLowPrices.push(rowItem.low_price);
-      }
-
-      for (let i = 0; i < lastDayHighPrices.length; i++) {
-        tempHigh += lastDayHighPrices[i];
-        tempLow += lastDayLowPrices[i];
-      }
-
-      highAverage = Math.floor(tempHigh / lastDayHighPrices.length);
-      lowAverage = Math.floor(tempLow / lastDayLowPrices.length);
-    });
-
-    db.get(sqlGrabRecent, [], (err, row) => {
-      if (err) console.log(err);
-      const milliseconds = row.timestamp;
-      const dateObject = new Date(milliseconds);
-      const humanDateFormat = dateObject.toLocaleString();
-      itemInfo.push(row);
-      item_name = row.item_name;
-      recentTime = humanDateFormat;
-
-      res.render("search", {
-        labels: itemTimestamp,
-        dataHighPrice: itemHighPrice,
-        dataLowPrice: itemLowPrice,
-        itemID: item_name,
-        recentTime: recentTime,
-        dataInfo: itemInfo,
-        highAvg: highAverage,
-        lowAvg: lowAverage,
-      });
-    });
-  });
-});
-
-app.get("/faqs", (req, res) => {
-  res.render("faqs");
-});
+// Handling GET request for the FAQ page
+app.get('/faqs', (req, res) => {
+    res.render('faqs');
+})
 
 app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  console.log(`Successfully connected at port ${PORT}`);
-});
+    if (err) console.log(err);
+    console.log(`Successfully connected at port ${PORT}`);
+})
